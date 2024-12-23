b69dcfed4f9109971dc71d35458b557541f1cc39
gravetogeek <edusrm11@gmail.com>
2024-12-23 10:20:27 -0300

    :sparkles: feat: Atualização do módulo app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {BarcodeModule} from '@/barcode/barcode.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {CommonModule} from './common/common.module';
+import {DatabaseModule} from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [BarcodeModule,DatabaseModule,CommonModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Habilitar validação global
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true, // Remove propriedades que não estão no DTO
+        forbidNonWhitelisted: true, // Gera erro para propriedades desconhecidas
+        transform: true, // Converte dados para os tipos do DTO
+      }));
   await app.listen(3000);
 }
 bootstrap();
